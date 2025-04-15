e186f251f06f3a9d40e6757091cae6f081f2536f
sam-yakymenko <sekazz24@gmail.com>
2025-04-15 14:49:34 +0300

    tel_book

--- lab_04_/tel_book/src/main.rs
+++ lab_04_/tel_book/src/main.rs
@@ -1,19 +1,25 @@
-use std::io;
-use std::io::BufRead;
-
 fn clean_phone_number(number: &str) -> String {
-    let mut cleaned_number = number.replace(|c: char| !c.is_numeric(), "");
-    if cleaned_number.starts_with("3") {
-        cleaned_number = cleaned_number[1..].to_string();
+    let mut cleaned_number: String = number
+        .chars()
+        .filter(|c| c.is_digit(10))
+        .collect();
+
+    if cleaned_number.len() == 12 && cleaned_number.starts_with("3") {
+        cleaned_number = cleaned_number[2..].to_string();
     }
+
     cleaned_number
 }
 
 fn main() {
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        let line = line.unwrap();
-        let cleaned_number = clean_phone_number(&line);
-        println!("{}", cleaned_number);
+    let numbers: Vec<&str> = vec![
+        "+3 (050)-995-0253",
+        "050-995-0253",
+        "3 050 995 0253",
+        "050.995.0253",
+    ];
+
+    for number in numbers {
+        println!("{} -> {}", number, clean_phone_number(number));
     }
 }
